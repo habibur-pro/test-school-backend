f01a5b4d15e2945a87bfd528cd3a3c17642c860e
Habibur Rahman <va.habibur@gmail.com>
2025-08-10 17:54:22 +0600

    fix: fix auth access token expairation

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -10,6 +10,8 @@ const config = {
     refresh_token_secret: process.env.REFRESH_TOKEN_SECRET as string,
     mailer_name: process.env.MAILER_NAME as string,
     mailer_pass: process.env.MAILER_PASS as string,
+    access_token_expires_in: process.env.ACCESS_TOKEN_EXPIRES_IN || '15m',
+    refresh_token_expires_in: process.env.REFRESH_TOKEN_EXPIRES_IN || '7d',
 }
 
 export default config
